b913ad4f78fe880917aaedcd44b67b7437bfc988
rinsarmu <roberaensermu@gmail.com>
2023-02-24 14:01:34 +0300

    starting our api handling using get request

--- part1/Natours-expres/app.js
+++ part1/Natours-expres/app.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express()
+const fs = require('fs');
 
-app.get('/',(req,res)=>{
-    res.send('Natours app start here')
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours.json`, 'utf8'))
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status:'success',
+        results:tours.length,
+        data:{
+            tours:tours 
+        }
+    })
 })
+
+// app.get('/',(req,res)=>{
+//     res.status(200).json({message:'Natours app  start here', app:"Natours"})
+// })
+
+// app.post('/',(req,res)=>{
+//     res.send("req.body is sent over")
+// })
 
 app.listen(8000, ()=>{console.log("port is running on 8000")})
