2c5496004e627628167df268a8edc937ac84d28f
rinsarmu <roberaensermu@gmail.com>
2023-02-26 12:41:42 +0300

    sorting, limiting fiels, filtering, and paginating of the data

--- part1/Natours-expres/controllers/tourController.js
+++ part1/Natours-expres/controllers/tourController.js
@@ -1,74 +1,33 @@
-const fs = require('fs');
 const Tour = require('../models/tourModels')
-// const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, 'utf8'))
-
-exports.getAllTours = async(req, res) => {
-    try{
-        // BUILD QUERY
-        // Filtering using where
-        // const tours = await Tour.find().where('duration').equals(5).where('difficulty').equals('easy')
-        console.log(req.query)
-
-        //1. Filtering
-        const queryObj = {...req.query}
-        const excludedFields = ['page', 'sort', 'limit','fields'] //excluded object when we filter the data
-        excludedFields.forEach(el => delete queryObj[el])
+const APIFeatures = require('./../utils/ApiFeatures');
 
 
-        //2. Advanced Filtering
-        let queryStr = JSON.stringify(queryObj)
-       queryStr = queryStr.replace(/\b(gte|lte|gt|lt)\b/g,match =>`$${match}`)
-        console.log("dd")
-        console.log(JSON.parse(queryStr))
-      
-        const query = await Tour.find(JSON.parse(queryStr));
-        
-        // Execute Query 
 
-        const tours = await query
-
-        //Send Response
-        res.status(200).json({
-            status:'success',
-            results:tours.length,
-            requestedAt: req.requestTime,
-            data:{
-                tours:tours 
-            }
-        })
-    }  catch(err){
-        res.status(500).json({
-            status: 'fail',
-            message: err
-        })
-        
+exports.getAllTours = async (req, res) => {
+    try {
+      // EXECUTE QUERY
+      const features = new APIFeatures(Tour.find(), req.query)
+        .filter()
+        .sort()
+        .limitFields()
+        .paginate();
+      const tours = await features.query;
+  
+      // SEND RESPONSE
+      res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+          tours
+        }
+      });
+    } catch (err) {
+      res.status(404).json({
+        status: 'fail',
+        message: err
+      });
     }
-}
-
-// exports.checkId = (req,res,next,val)=>{
-//     const {id} = req.params;
-//     let tour = tours.find(el=> el.id == id)
-//     if(!tour){
-//        return res.status(404).json({
-//             status: 'fail',
-//             message: 'tour not found'
-//         })
-//     }
-//     next();
-// }
-
-// exports.checkBody =(req,res,next)=>{
-//     const {name,price} = req.body;
-//     if(!name || !price){
-//         return res.status(400).json({
-//             status: 'fail',
-//             message: 'Missing name or price'
-//         })
-
-//     }
-//     next()
-
-// }
+  };
 
 exports.getTour = async (req, res) => {
     try{
