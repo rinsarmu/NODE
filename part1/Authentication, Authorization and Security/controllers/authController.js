6e434a82bb5c49a11d7e3bbd66e45309d333701b
rinsarmu <roberaensermu@gmail.com>
2023-02-28 23:12:33 +0300

    hashing the password

--- part1/Authentication, Authorization and Security/controllers/authController.js
+++ part1/Authentication, Authorization and Security/controllers/authController.js
@@ -4,6 +4,7 @@ const User = require('../models/userModels')
 const catchAsync = require('./../utils/catchAsync')
 
 exports.signup = catchAsync(async(req,res,next)=>{
+    console.log(req.body)
     const newUser = await User.create(req.body)
     res.status(201).json({
         status: 'Success',
--- part1/Authentication, Authorization and Security/models/userModels.js
+++ part1/Authentication, Authorization and Security/models/userModels.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
+const validators = require('validator')
+const bcrypt = require('bcryptjs')
 
 const userSchema  = new mongoose.Schema({
     name:{
@@ -12,27 +13,59 @@ const userSchema  = new mongoose.Schema({
         lowercase: true,
 
         required: [true, "Please provide your email"],
-        validate: [validator.isEmail, 'Email must be a valid email address']
+        validate: [validators.isEmail, 'Email must be a valid email address']
     },
-    photo: String,
+    photo: {
+        type: String,
+        defaultValue:"profile"
+
+    },
+
     password:{
         type: String,
         required: [true, 'please provide us a password'],
         minlength: 6,
-        select: false
     },
-    passwordConfirm:{
+    date:String,
+    confirmPassword:{
         type: String,
-        validate: {
-            validator: function(val){
-            return val == this.password
+        validate:{
+            validator: function(el){
+                return el === this.password
             },
-            message: 'Please confirm a password'
+            message: 'Please confirm your password'
         }
-        
     }
 
+  
+
+
+
+    // passwordConfirm:{
+    //     type: String,
+    //     validate: {
+    //         validator: function(val){
+    //             console.log("something ....")
+    //         return val == this.password
+    //         },
+    //         message: 'Please confirm a password'
+    //     }
+        
+    // }
+
     
+})
+
+userSchema.pre('save',async function(next){
+    //Only run this functin if password was actually modified
+   if(!this.isModified('password')) return next()
+
+   // Hash th password with cost of 12
+   this.password = await bcrypt.hash(this.password, 12)
+
+   //Delete password confirm field
+   this.confirmPassword = undefined
+   next()
 })
 
 const User = mongoose.model('User', userSchema)
