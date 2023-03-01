83e2a5cdf8f32238872dcd310dbed44a7eb282a2
rinsarmu <roberaensermu@gmail.com>
2023-03-01 06:31:57 +0300

    crating JWT for a newly created user for login as soon as he signed up

--- part1/Authentication, Authorization and Security/controllers/authController.js
+++ part1/Authentication, Authorization and Security/controllers/authController.js
@@ -1,13 +1,23 @@
 // const User = require('./../models/userModels')
 const User = require('../models/userModels')
+const jwt = require('jsonwebtoken')
 
 const catchAsync = require('./../utils/catchAsync')
 
 exports.signup = catchAsync(async(req,res,next)=>{
     console.log(req.body)
-    const newUser = await User.create(req.body)
+    const newUser = await User.create({
+        name:req.body.name,
+        email: req.body.email,
+        password:req.body.password
+    })
+
+    const token = jwt.sign({id:newUser._id}, process.env.JWT_SECRET,{
+        expiresIn: process.env.JWT_EXPIRES_IN
+    })
     res.status(201).json({
         status: 'Success',
+        token,
         data:{
             user: newUser
         }
