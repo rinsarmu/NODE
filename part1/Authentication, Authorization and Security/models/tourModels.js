ed535c1336a8000699148787c4d878957c9f991b
rinsarmu <roberaensermu@gmail.com>
2023-03-03 16:08:47 +0300

    modelling tour guide child referencing

--- part1/Authentication, Authorization and Security/models/tourModels.js
+++ part1/Authentication, Authorization and Security/models/tourModels.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+// const User = require("./userModels")
 // const validator = require('validator')
 
 const tourSchema = new mongoose.Schema({
@@ -81,7 +82,41 @@ const tourSchema = new mongoose.Schema({
     secretTour:{
         type: Boolean,
         default: false
-    }
+    },
+    startLocation : {
+        //GeoJson
+        type:{
+            type: String,
+            default: 'Point',
+            enum:['Point']
+
+            
+        },
+        coordinates:[Number],
+        address: String,
+        description: String
+    },
+    locations:[
+        {
+            type: {
+                type: String,
+                default: 'Point',
+                enum: ["Point"]
+            },
+            coordinates: [Number],
+            address: String,
+            description: String,
+            day: Number,
+        }
+    ],
+    // guides:Array
+    guides:[
+        {
+            type: mongoose.Schema.ObjectId,
+            ref:'User'
+        }
+    ]
+
 
 },
 {
@@ -101,6 +136,18 @@ tourSchema.pre('save', function(next){
     next();
 
 })
+
+// embeding users to guide
+// tourSchema.pre('save', async  function(){
+//     console.log(this.guides)
+//     const guidesPromises = this.guides.map(async id=>await User.findById(id))
+
+//    this.guides = await Promise.all(guidesPromises)
+// })
+
+
+
+
 
 // tourSchema.pre('save', function(next){
 //     console.log("WIll save documetn...")
