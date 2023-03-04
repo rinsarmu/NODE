c0b768a30c05203b519ee4c36930c789257b44e7
rinsarmu <roberaensermu@gmail.com>
2023-03-04 15:02:12 +0300

    Adding nested routes

--- part1/Authentication, Authorization and Security/routes/reviewRoutes.js
+++ part1/Authentication, Authorization and Security/routes/reviewRoutes.js
@@ -7,15 +7,14 @@ const {
 
 const {protect, restrictTo} =  require('./../controllers/authController')
 
-const reviewRouter = express.Router()
+const reviewRouter = express.Router({mergeParams:true})
+console.log("hhhhhhhhhhhhhhhhhhhhhhhh")
 
-
-reviewRouter.route('/:id')
-    .get(getReview)
+// reviewRouter.route('/:id')
+//     .get(getReview)
 
 reviewRouter.route('/')
     .get(protect,restrictTo('user'), getAllReviews)
     .post(protect,restrictTo('user'), createReviews)
-console.log("hhkk")
 
 module.exports = reviewRouter
--- part1/Authentication, Authorization and Security/routes/tourRoutes.js
+++ part1/Authentication, Authorization and Security/routes/tourRoutes.js
@@ -16,8 +16,12 @@ const {
     } = require('../controllers/tourController')
 
 const {protect, restrictTo} = require('./../controllers/authController')
-const {createReviews} = require('./../controllers/reviewController')
+// const {createReviews} = require('./../controllers/reviewController')
+const reviewRouter = require('./../routes/reviewRoutes');
+const { Router } = require('express');
 
+
+tourRouter.use('/:tourId/review', protect, reviewRouter)
 
 // tourRouter.param('id', checkId)
 tourRouter.route('/tour-stats').get(getTourStats)
@@ -33,5 +37,6 @@ tourRouter.route('/:id')
     .patch(updateTour)
     .delete(protect, restrictTo("admin", 'lead-guide'),deleteTour)
 
-tourRouter.route('/:tourId/review').post(protect, restrictTo('user'), createReviews)
-    module.exports = tourRouter
+// tourRouter.route('/:tourId/review').post(protect, restrictTo('user'), createReviews)
+
+module.exports = tourRouter
