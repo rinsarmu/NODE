1ec65b8c8f09849064ef412a6485b3e0e4771e98
rinsarmu <roberaensermu@gmail.com>
2023-03-18 16:48:44 +0300

    part1/Natours-expres/node_modules/utils-merge/package.json

--- part1/Authentication, Authorization and Security/models/reviewModel.js
+++ part1/Authentication, Authorization and Security/models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Tour = require('./tourModels')
 
 const reviewSchema = new mongoose.Schema({
     review: {
@@ -40,16 +41,41 @@ reviewSchema.pre(/^find/, function(next){
 
     //Populating the reviews
     this
-    .populate({
-        path: 'tour',
-        select: 'name'
-    })
+    // .populate({
+    //     path: 'tour',
+    //     select: 'name'
+    // })
     .populate({
         path: 'user',
         select: "name, role"
     })
 
     next()
+})
+
+reviewSchema.statics.calcAverageRatings =async function(tourId){
+   const stats =  await this.aggregate([
+        {
+            $match: {tour: tourId}
+        },
+        {
+            $group:{
+                _id: '$tour',
+                nRating: {$sum: 1},
+                avgRating:{$avg: '$rating'}
+            }
+        }
+    ])
+    await Tour.findByIdAndUpdate(tourId,{
+        ratingsAverage: stats[0].avgRating,
+        ratingsQuantity: stats[0].nRating
+    })
+}
+
+reviewSchema.post('save', function(){
+    //this points to current review
+    this.constructor.calcAverageRatings(this.tour)
+    
 })
 
 const Review = mongoose.model('Review', reviewSchema)
