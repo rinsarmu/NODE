19b44d0c887b30b13490e9bd6aa6a52586ea4dbf
rinsarmu <roberaensermu@gmail.com>
2023-03-25 23:49:25 +0300

    building the tour template

--- part1/Authentication, Authorization and Security/controllers/viewController.js
+++ part1/Authentication, Authorization and Security/controllers/viewController.js
@@ -1,14 +1,20 @@
+const catchAsync = require('./../utils/catchAsync')
+const Tour = require('../models/tourModels')
+
+
 exports.getBase = (req,res,next)=>{
     res.status(200).render('base', {
         text: 'this is natours app'
     })
 }
 
-exports.getOverview =  (req,res,next)=>{
+exports.getOverview = catchAsync( async (req,res,next)=>{
+    const tours = await Tour.find()
     res.status(200).render('overview', {
-        title: 'All tours'
+        tours
     })
 }
+)
 
 exports.getTour =  (req,res,next)=>{
     res.status(200).render('tour', {
